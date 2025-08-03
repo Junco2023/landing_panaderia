9ca1e14fb48a38c251fe9b02a8408fea65436c8e
Matias <gonzalezmatias6164@gmail.com>
2025-08-03 19:02:25 -0300

    Subida inicial de landig page -panaderia artesanal

---
+++ js/index.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+  //sidenav
+  const sidenavs = document.querySelectorAll('.sidenav');
+  var instanciaSidenav = M.Sidenav.init(sidenavs)
+  //slider
+  const slider = document.querySelectorAll('.slider');
+  var instanciaSlider = M.Slider.init(slider, {
+    indicators: true,
+    height: 650,
+    duration: 500,
+    interval: 4000
+  })
+  //tooltip
+  const tool=document.querySelectorAll('.tooltipped');
+  var instaniaTooltip=M.Tooltip.init(tool,{
+    inDuration:300,
+    transitionMovement:1
+  });
+
+  //cerrar sidenav al hacer click en enlace
+  const sidenavsLinks = document.querySelectorAll('.sidenav a');
+  sidenavsLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      const sidenavInstance = M.Sidenav.getInstance(document.querySelector('.sidenav'));
+      if (sidenavInstance) {
+        sidenavInstance.close();
+      }
+    });
+  });
+  /*pwa condig*/
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/sw.js")
+        .then(registration => {
+          console.log("Service Worker registrado con éxito:", registration.scope);
+        })
+        .catch(error => {
+          console.log("Fallo al registrar el Service Worker:", error);
+        });
+    });
+  }
+  
+});
---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/materialize/css/materialize.min.css",
+  "/materialize/js/materialize.min.js",
+  "/js/index.js",
+  "/css/index.css",
+  "/img/slider/huevos-harina.jpg",
+  "/img/iconos/facebook.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        // Si no hay red y la petición es para una página HTML, muestra offline.html
+        if (event.request.destination === 'document') {
+          return caches.match('/offline.html');
+        }
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
